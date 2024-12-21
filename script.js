d09445858db9bc3a24f8dd6050dc953f76c1a52f
Adil Ferryat <aferryat@e3r7p9.1337.ma>
2024-12-21 15:22:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const writeUp = document.querySelector("input");
+const btn = document.querySelector("button");
+const x = document.querySelector(".x");
+
+// function to save our datat in localstorage
+
+function saveData()
+{
+	localStorage.setItem("data", x.innerHTML);
+}
+
+// function to display our saved data
+
+function dispaly_data()
+{
+	x.innerHTML = localStorage.getItem("data");
+}
+// function to create new child and add it to our list
+
+function add_list(text)
+{
+	let new_list = document.createElement("li");
+	new_list.innerHTML = text;
+	x.appendChild(new_list);
+	saveData();
+}
+
+//remove child if we cliked on it 
+x.addEventListener("click", (e)=>
+{
+	let v = e.target;
+	x.removeChild(v);
+	saveData();
+})
+
+// main function to create new child and make new note
+btn.addEventListener("click", () =>
+{
+	text = writeUp.value;
+	if (text != "")
+		add_list(text);
+	else
+		alert("pliise try your best to make sure you field evething :)")
+});
+
+// load a saved data
+dispaly_data();
